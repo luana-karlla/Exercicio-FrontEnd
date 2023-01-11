ee2639dd4bc2cb0ab822bbe92f7092b2ad7b83a0
Luana Karlla Silva <90729575+luana-karlla@users.noreply.github.com>
2023-01-11 11:30:35 -0300

    Calculo do m^2 da parede JS

--- parede/parede01.js
+++ parede/parede01.js
@@ -1,9 +1,13 @@
 
- let valorLargura = parseFloat(document.getElementById('l').value);
- let valorAltura = parseFloat(document.getElementById('a').value);
-let button = document.getElementById('button');
-
 function calcular(){
+
+    let valorLargura = parseFloat(document.getElementById('l').value);
+    let valorAltura = parseFloat(document.getElementById('a').value);
+    let resposta = document.getElementById('res');
+ 
     valorArea = valorAltura * valorLargura //b x a
     qtinta = valorArea * 2;
+
+    resposta.innerHTML = `Á área total é de ${valorArea}m², e a quantidade de tinta a uma área de 2m² é de ${qtinta} litros`;
+
 }
